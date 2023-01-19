0e70d12a56d32cc7201aba221a9b8c941d6f57ea
Pierre-marie LAM <plam@e1r8p6.clusters.42paris.fr>
2023-01-19 15:51:16 +0100

    minor modifications on message.hpp, to be discussed later for other components interactions

--- INCL/Message.hpp
+++ INCL/Message.hpp
@@ -6,12 +6,12 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 17:40:50 by plam              #+#    #+#             */
-/*   Updated: 2023/01/17 17:58:34 by plam             ###   ########.fr       */
+/*   Updated: 2023/01/19 15:47:18 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef __MESSAGES_HPP__
-# define __MESSAGES_HPP__
+#ifndef __MESSAGE_HPP__
+# define __MESSAGE_HPP__
 
 # include "User.hpp"
 # include "Server.hpp"
@@ -23,7 +23,10 @@ class Message {
 		Message();
 		~Message();
 		Message(const Message &other);
-		&operator=(const Message &other);
+		Message	&operator=(const Message &other);
+		
+		void	SendPvtMess(User Usr1, User Usr2, std::string Mess);	// sending private message from a user to another ?
+		void	SendCnlMess(User Usr, Channel Cnl, std::string Mess);	// sending message in/to a channel ?
 };
 
 #endif
