6c2be9c40f2905b34e1727f747ba401692522521
Pierre-marie LAM <plam@e1r7p9.clusters.42paris.fr>
2023-01-27 16:22:07 +0100

    Command.hpp started, need improvements on the messages treatment and 'parsing' from cmd to msg if correct

--- SRCS/Channel.cpp
+++ SRCS/Channel.cpp
@@ -6,8 +6,20 @@
 /*   By: plam <plam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 16:37:57 by plam              #+#    #+#             */
-/*   Updated: 2023/01/12 16:38:14 by plam             ###   ########.fr       */
+/*   Updated: 2023/01/27 16:22:03 by plam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../INCL/Channel.hpp"
+
+Channel::Channel() { }
+Channel::Channel(const Channel &other) {
+	*this = other;
+}
+
+Channel::~Channel();
+
+Channel::Channel	&operator=(const Channel &other);
+
+void	Channel::add(User &user);
+void	Channel::del(User &user);
